e0160c61d772d80aeb33a59c857bf1e8f4be6742
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2022-11-11 17:46:39 -0800

    Add files via upload

--- edinburgh.py
+++ edinburgh.py
@@ -2,24 +2,13 @@ import numpy as np
 import pandas as pd
 from pathlib import Path 
 
-"""
-TODO: Create total score on Edinburgh scale
-
-X Frequency Table of total scores ()
-X for every score, the number of people who scored each 
-
-Depression and Anxiety combination: get
-average and range of depression and anxiety range
-"""
-
 csv1 = Path(__file__).with_name('V1C.CSV') #fixed so you dont have to change address
 df = pd.read_csv(csv1)
 
-print('\n Edinburgh Postnatal Depression Scale - #ppl for every score \n')
-dict1 = {'ID' : ['V1CA01','V1CA02','V1CA03','V1CA04','V1CA05','V1CA06','V1CA07','V1CA08','V1CA09','V1CA10'], 1:[],2:[],3:[],4:[]}
-temp = []
-for i in range(1,11):
-    for j in range(1,5):
+print('\n Edinburgh Postnatal Depression Scale - #ppl that answered each checkbox \n')
+dict1 = {'varID' : ['V1CA01','V1CA02','V1CA03','V1CA04','V1CA05','V1CA06','V1CA07','V1CA08','V1CA09','V1CA10'], 1:[],2:[],3:[],4:[]}
+for i in range(1,11): #1-10
+    for j in range(1,5): #Questions 1-4
         if(i>=10):
             dict1[j].append(len(df[(df['V1CA{}'.format(i)] == j)]))
         else:
@@ -27,29 +16,24 @@ for i in range(1,11):
 dict1 = pd.DataFrame(dict1)
 print(dict1,'\n')
 
+# TODO: EDINBURGH TOTAL SCORES
+
+def f(row):
+    score = 0
+    for i in range(1,11):
+        if(i== 10):    
+            score += abs(row['V1CA10'] - 4)
+        elif(i == 1 or i == 2 or i == 4):
+            score += row['V1CA0{}'.format(i)] - 1
+        else: score += abs(row['V1CA0{}'.format(i)] - 4)
+    return score
+
+df['TotalScore'] = df.apply(f, axis=1)
+print(df.head())
+print('\n Mean Total Score:', df['TotalScore'].mean() ,'\n')
 
 
 
 
-# Both below work just wanted correct format and not just value_counts for each
-"""
-for i in range(1,11):
-    if(i>=10):
-        print(df['V1CA{}'.format(i)].value_counts())
-    else:
-        print(df['V1CA0{}'.format(i)].value_counts())
-    print()
-print()
-"""
 
-"""
-for i in range(1,11):
-    for j in range(1,5):
-        if(i>=10):
-            print('#ppl scored {} V1CA{}:'.format(j,i), len(df[(df['V1CA{}'.format(i)] == j)]),'', end ='')
-        else:
-            print('#ppl scored {} V1CA0{}:'.format(j,i), len(df[(df['V1CA0{}'.format(i)] == j)]),'', end ='')
-    print()
 
-print()
-"""
