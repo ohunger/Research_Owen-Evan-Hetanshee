7a9f195269d587e8b059962211b245217f4b65d1
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2022-11-24 12:41:18 -0800

    Add files via upload

--- edin-stai.py
+++ edin-stai.py
@@ -3,10 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path 
 
-csv1 = Path(__file__).with_name('V1C.CSV') 
+csv1 = Path(__file__).with_name('V1C.CSV')  #EDIN
 dfe = pd.read_csv(csv1)
-csv2 = Path(__file__).with_name('V1H.CSV') 
+csv2 = Path(__file__).with_name('V1H.CSV')  #STAI
 dfs = pd.read_csv(csv2)
+csv3 = Path(__file__).with_name('CMA.CSV')  #3dep3anxiety
+dfp = pd.read_csv(csv3)
 
 
 def freqTable(): #crappy frequency table for edinburgh
@@ -44,41 +46,59 @@ def v1(row): # TODO: || STAI || TOTAL SCORES COLUMN
                 else: score += row['V1HA0{}'.format(i)]
     return score
 
-def subEdinburgh(row): # || EDINBURGH ||
+def subEdinburgh(row): # || EDINBURGH || subscore if>=10: 1, 0 otherwise
     score = 0
-    if(row['TotalScore'] >= 10): score = 1
+    if(row['TotalEDINScore'] >= 10): score = 1
     return score
 
-def suicidality(row): # || EDINBURGH ||
+def suicidality(row): # || EDINBURGH || #want to double check with prof
     score = 0
     if(row['V1CA10']<=3): score = 1
     return score
     
-def subSTAI(row): # || STAI ||
+def subSTAI(row): # || STAI ||  subscore if>=40: 1, 0 otherwise
     score = 0
-    if(row['TotalScore'] >= 40): score = 1
+    if(row['TotalSTAIScore'] >= 40): score = 1
     return score
 
 def histo(): # TODO: HISTOGRAMS void function
-    hist1 = dfe.hist(column='TotalScore')
+    hist1 = dfe.hist(column='TotalEDINScore')
     plt.title('Edinburgh TotalScore')
-    hist2 = dfs.hist(column='TotalScore')
+    hist2 = dfs.hist(column='TotalSTAIScore')
     plt.title('STAI TotalScore')
     plt.show()
 
+def newSet(row): #Trying to make descriptive 
+
+    pass
+
+
 def main():
     #freqTable()
-    print('\n')
-    dfe['TotalScore'] = dfe.apply(f, axis=1)    
-    dfe['SubScore'] = dfe.apply(subEdinburgh, axis=1)
+    dfe['TotalEDINScore'] = dfe.apply(f, axis=1)    
+    dfe['SubEDINScore'] = dfe.apply(subEdinburgh, axis=1)
     dfe['Suicidality'] = dfe.apply(suicidality, axis=1)
-    print(dfe.head())
+    #print('\n Edinburgh \n' )
+    #print(dfe.head())
 
-    dfs['TotalScore'] = dfs.apply(v1, axis=1) 
-    dfs['SubScore'] = dfs.apply(subSTAI, axis=1)
-    print(dfs.head())
+    
+    dfs['TotalSTAIScore'] = dfs.apply(v1, axis=1) 
+    dfs['SubSTAIScore'] = dfs.apply(subSTAI, axis=1)
+    #print('\n STAI \n')
+    #print(dfs.head())
 
-    #print('\n #ppl endorsed item 10 Edinburgh:', len(df[df['V1CA10'] <= 3]), '\n')
+    print('\n   GUIDE:')
+    print('SubEdinScore (1 if>=10 )   SubSTAIScore (1 if>=40)  Suicidality(endorsed #10 Edinburgh')
+    print('CMAE04a1 (a,b,c) = 1 if treated for depression.  CMAE04a2 (a,b,c) = 1 if treated for anxiety \n')
+    
+
+    dff = pd.DataFrame()
+    dff = pd.merge(pd.merge(dfe,dfs, on="PublicID"),dfp,on="PublicID")
+    dff = dff[['PublicID', 'SubEDINScore', 'SubSTAIScore', 'Suicidality', 'CMAE04a1a', 'CMAE04a1b','CMAE04a1c','CMAE04a2a','CMAE04a2b','CMAE04a2c']]
+    #print(len(dff['PublicID']))
+    print(dff.head())
+
+
 
 
 main()
