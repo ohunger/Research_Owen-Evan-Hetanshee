0c0b055bfd108c6caa97afe70897c6d5a1415dbc
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2023-01-11 22:19:04 -0800

    Add files via upload

--- pca_Take2.py
+++ pca_Take2.py
@@ -25,13 +25,12 @@ plt.plot(range(1,13), pca.explained_variance_ratio_.cumsum(), marker = 'o', line
 plt.title('Explained Variance by Componenets')
 plt.xlabel("Num Componenents")
 plt.ylabel('Cumulative Explained Variance')
-plt.show()
+#plt.show()
 
 pca = PCA(n_components=7)
 pca.fit(x)
 scoresPCA = pca.transform(x)
 
-kmeans_model = KMeans()
 wcss = [] #sum of square
 for i in range(1,10):
     kmeans_pca = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
@@ -43,6 +42,14 @@ plt.plot(range(1,10), wcss, marker = 'o', linestyle = '--')
 plt.title('optimal clusters')
 plt.xlabel("Num Clusters")
 plt.ylabel('wcss')
-plt.show()
+#plt.show()
 
+"""
+kmeans_pca = KMeans(n_clusters = 2, init = 'k-means++', random_state=42)
+kmeans_pca.fit(scoresPCA)
 
+dfkmeans = pd.concat([df.reset_index(drop=True),pd.DataFrame(scoresPCA)], axis=1)
+dfkmeans.columns.values[-3: ] = ['Component 1', 'Component 2', 'Component 3']
+dfkmeans['Segment K-means'] = kmeans_pca.labels_
+print(dfkmeans.head())
+"""
