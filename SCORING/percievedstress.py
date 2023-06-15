2d519789c6c968cce9785f9657aa98a8d0149015
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2023-06-14 21:46:31 -0700

    Add files via upload

--- SCORING/percievedstress.py
+++ SCORING/percievedstress.py
@@ -40,5 +40,3 @@ df = df[['PublicID', 'TotalScore', 'StressLevel']]
 print(df)
 stress_counts = df['StressLevel'].value_counts()
 print(stress_counts)
-discrimination_counts = df['DiscriminationLevel'].value_counts()
-print(discrimination_counts)
